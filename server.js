34a4f5a90578642c47fe67fe287427e9bd980a7f
Andrew Moroco <andrew.moroco@gmail.com>
2020-08-07 16:59:55 -0700

    initial

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const todoRouter = require('./routes/todo-router');
+
+const app = express();
+require('dotenv').config();
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        appName: 'todos',
+    });
+});
+
+app.use('/todos', todoRouter);
+
+app.use('*', (req, res) => {
+    res.status(404).send({
+        error: `Not Found`,
+    });
+});
